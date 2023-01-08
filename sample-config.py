6dfb51abe0bfc07467099b3bc6d7ea4995466848
Doug Martin <33435021+spidermedic@users.noreply.github.com>
2023-01-08 18:18:33 -0500

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -1,4 +1,4 @@
-# Fill in all of the information below and rename this file to 'config.py
+# Fill in all of the information below and rename this file to config.py
 
 # Twitter developer keys
 API_KEY = "YOUR API KEY"
@@ -9,12 +9,12 @@ ACCESS_TOKEN_SECRET = "YOUR ACCESS TOKEN SECRET"
 CLIENT_ID = "YOUR CLIENT ID"
 CLIENT_SECRET = "YOUR CLIENT SECRET"
 
-# Twitter user ID
+# Your actuall twitter user ID
 USER_ID = "1234567890000000000"
 # Redirect URL
 URL = "https://example.com"
 
-# Removing any of these items will break the program
+# Removing any of the scope items will break the program.
 SCOPE = (
     [
         "tweet.read",
@@ -32,8 +32,8 @@ SCOPE = (
     ],
 )
 
-# Accounts in this list will never be purged.
-# The tuple format it (account_name, account_id)
+# This list containts accounts that will never be purged. The two accounts listed are samples and can be changed or deleted.
+# Each line is a tuple using the format: (account_name, account_id)
 PROTECTED_FOLLOWS = [
     ("BetteMidler", 139823781),
     ("SteveMartinToGo", 14824849),
