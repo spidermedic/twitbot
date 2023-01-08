ddcb322d01c8530345f11f107da342801f3e3f2b
Doug Martin <33435021+spidermedic@users.noreply.github.com>
2023-01-08 18:19:22 -0500

    Update sample-auth.py

--- sample-auth.py
+++ sample-auth.py
@@ -1,5 +1,5 @@
-# fill in with your tokens and rename to 'auth.py'
+# Fill in with your tokens and rename to auth.py
 access_token = "YOUR ACCESS TOKEN"
 refresh_token = "YOUR REFRESH TOKEN"
-# leave this alone as the program will write the proper value
+# Leave the expiration alone as the program will write the proper value
 expires = 0
